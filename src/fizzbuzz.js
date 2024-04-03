89e643a31a4a3d1011521f6a05ca6111c0d5fea6
Justinas <gercasjustinas@gmail.com>
2024-04-03 20:03:07 +0200

    arrays added

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,9 @@
 // TODO: Write your function in this file
 function fizzBuzz(lower, upper) {
-    
+if ()
+  lower = 1, 2, 4, 7, 8, 11, 13, 14, 17, 19, 22
+  upper = 26, 28, 29, 31, 32, 34, 37, 38
+  numberArr = [lower, upper, 3, 5, 6, 9, 10, 12, 15, 18, 20, 21, 24, 25, 27, 30, 33, 35, 36, 39, 40]
 }
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
