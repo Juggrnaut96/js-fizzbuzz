4960f9ef6a9fe6751233e9ebed5fb270e40fc77b
Justinas <gercasjustinas@gmail.com>
2024-04-03 20:34:34 +0200

    numberArr adjusted

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,11 @@
 // TODO: Write your function in this file
 function fizzBuzz(lower, upper) {
-if ()
-  lower = 1, 2, 4, 7, 8, 11, 13, 14, 17, 19, 22
-  upper = 26, 28, 29, 31, 32, 34, 37, 38
-  numberArr = [lower, upper, 3, 5, 6, 9, 10, 12, 15, 18, 20, 21, 24, 25, 27, 30, 33, 35, 36, 39, 40]
+  //   lower = [1, 2, 4, 7, 8, 11, 13, 14, 17, 19, 22]
+  //   upper = [26, 28, 29, 31, 32, 34, 37, 38]
+  numberArr = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40
+  ]
 }
 // TODO: Change undefined below to the name of your function
 module.exports = fizzBuzz
