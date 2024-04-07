54a4fde5410c1de9d4bc65b19d39fc4002cc52a9
Justinas <gercasjustinas@gmail.com>
2024-04-07 19:17:40 +0200

    stuck at solving issues

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,11 +1,22 @@
 // TODO: Write your function in this file
 function fizzBuzz(lower, upper) {
-  const addNumbers = Array.from(
+  addNumbers = Array.from(
     { length: upper - lower + 1 },
     (_, index) => index + lower
   )
+  for (let i = lower; i <= upper; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
   return addNumbers
 }
-console.log(fizzBuzz(1, 100))
+console.log(fizzBuzz(1, 54))
 // TODO: Change undefined below to the name of your function
 module.exports = fizzBuzz
